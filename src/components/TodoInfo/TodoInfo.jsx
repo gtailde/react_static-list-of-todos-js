d3ac9570539586a0bf6a2d14f1326d71dd4e9325
Oleg Nepiuk <gtailde@gmail.com>
2025-05-22 09:10:11 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import './TodoInfo.scss';
+import clsx from 'clsx';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={clsx('TodoInfo', todo.completed && 'TodoInfo--completed')}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <a className="UserInfo" href={todo.user.email}>
+      {todo.user.name}
+    </a>
+  </article>
+);
